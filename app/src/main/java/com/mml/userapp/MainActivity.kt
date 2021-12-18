2d224a9f90bbc3f912c9e87a1ddc205bbdfce921
theintmyatnoe.sbs <theintmyatnoe.sbs@gmail.com>
2021-12-18 17:37:14 +0630

    add progress

--- app/src/main/java/com/mml/userapp/MainActivity.kt
+++ app/src/main/java/com/mml/userapp/MainActivity.kt
@@ -3,18 +3,21 @@ package com.mml.userapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders.of
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.GsonBuilder
 import com.mml.userapp.adapters.UserListAdapter
+import com.mml.userapp.common.Utils
 import com.mml.userapp.data_access.DatabaseAccess
 import com.mml.userapp.listeners.AdapterCallback
 import com.mml.userapp.models.User
 import com.mml.userapp.viewmodels.UserListViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.progressDialog
 import org.jetbrains.anko.uiThread
 import java.io.Serializable
 
@@ -26,31 +29,46 @@ class MainActivity : AppCompatActivity(), AdapterCallback {
         setContentView(R.layout.activity_main)
         supportActionBar !!.title= "User List"
         val viewModel = of(this).get(UserListViewModel::class.java)
-        viewModel.connectApi()
-        viewModel.getUsers().observe(this, Observer {
-            if(it!!.isNotEmpty())
-            {
-                try{
-                    userList=it
-                    val gson= GsonBuilder().create()
-                    val db= DatabaseAccess.getAppDatabase(this)!!.userDao()
-                    db.insert(userList)
+        val progressDialog = Utils.makeProgressDialog(this, "Loading.....")
+        progressDialog.show()
+        if(!Utils.isNetworkConnected(applicationContext)!!){
+            Toast.makeText(applicationContext,"Please check your internet connection",Toast.LENGTH_SHORT).show()
 
-                    var list= listOf<User>()
-                    doAsync {
-                        list=db.getAllUser()
-                        uiThread {
-                            setRecyclerView(list)
+            progressDialog.show()
+        }
+
+        else{
+
+            viewModel.connectApi()
+            viewModel.getUsers().observe(this, Observer {
+                if(it!!.isNotEmpty())
+                {
+                    try{
+                        userList=it
+                        progressDialog.dismiss()
+                        val gson= GsonBuilder().create()
+                        val db= DatabaseAccess.getAppDatabase(this)!!.userDao()
+                        db.insert(userList)
+
+                        var list= listOf<User>()
+                        doAsync {
+                            list=db.getAllUser()
+                            uiThread {
+                                setRecyclerView(list)
+                            }
                         }
+                    }catch (e:Exception){
+                        throw e
+                        progressDialog.dismiss();
                     }
-                }catch (e:Exception){
-                    throw e
                 }
-            }
-        })
+            })
+        }
+
     }
     private fun setRecyclerView(list: List<User>) {
         try {
+
             adapter = UserListAdapter(list,this)
             recyclerView.layoutManager =
                 LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
--- app/src/main/java/com/mml/userapp/common/Utils.kt
+++ app/src/main/java/com/mml/userapp/common/Utils.kt
@@ -3,13 +3,24 @@ package com.mml.userapp.common
 import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Build
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 
+@Suppress("DEPRECATION")
 class Utils {
-
+companion object
+{
     private var myPermissions = arrayOf(
         Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.INTERNET
@@ -28,4 +39,64 @@ class Utils {
             }
         }
     }
+    //Check network connection
+    fun isNetworkConnected(context : Context) : Boolean?
+    {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnected
+    }
+
+    //show progress dialog
+    fun makeProgressDialog(context : Context, msg : String) : AlertDialog
+    {
+        val llPadding = 30
+        val ll = LinearLayout(context)
+        ll.orientation = LinearLayout.HORIZONTAL
+        ll.setPadding(llPadding, llPadding, llPadding, llPadding)
+        ll.gravity = Gravity.CENTER
+        var llParam = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        llParam.gravity = Gravity.CENTER
+        ll.layoutParams = llParam
+        val progressBar = ProgressBar(context)
+        progressBar.isIndeterminate = true
+        progressBar.setPadding(0, 0, llPadding, 0)
+        progressBar.layoutParams = llParam
+        progressBar.setBackgroundColor(Color.TRANSPARENT)
+
+        llParam =
+            LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        llParam.gravity = Gravity.CENTER
+        val tvText = TextView(context)
+        tvText.text = msg
+        tvText.setTextColor(Color.parseColor("#000000"))
+        tvText.textSize = 20f
+        tvText.setBackgroundColor(Color.TRANSPARENT)
+        tvText.layoutParams = llParam
+
+        ll.addView(progressBar)
+        ll.addView(tvText)
+        val builder = AlertDialog.Builder(context)
+        builder.setCancelable(false)
+        builder.setView(ll)
+        val dialog = builder.create()
+        val window = dialog.window
+        if (window != null)
+        {
+            val layoutParams = WindowManager.LayoutParams()
+            layoutParams.copyFrom(dialog.window !!.attributes)
+            layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+            layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT
+            dialog.window !!.attributes = layoutParams
+        }
+        return dialog
+    }
+}
+
 }
